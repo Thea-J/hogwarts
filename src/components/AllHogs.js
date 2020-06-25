f05746966f29448a3646f3237f4255f0b911844f
github username <github email address>
2020-06-25 17:42:59 +0100

    set us index component

---
+++ src/components/AllHogs.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+import "../porkers_data.js";
+
+class AllHogs extends Component {
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default AllHogs;
--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
 import HelloWorld from "./HelloWorld";
+import AllHogs from "./AllHogs";
 
 class App extends Component {
+  //We can define code here
   render() {
     return (
       <div className="App">
         <Nav />
         <HelloWorld />
+        <AllHogs />
       </div>
     );
   }
